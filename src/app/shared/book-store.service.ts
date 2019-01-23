7cc551e5ee56e99c39402b0a9056dbff3398960b
Maximilian Becher <maximilian.becher@ueberbit.de>
2019-01-24 00:54:43 +0100

    book-store: update getAll, getSingle to use Observables (API); add errorHandler

--- src/app/shared/book-store.service.ts
+++ src/app/shared/book-store.service.ts
@@ -1,18 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { retry, map, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+import { Book } from './book';
+import { BookRaw } from './book-raw';
+import { BookFactory } from './book-factory';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookStoreService {
+  private api = 'https://book-monkey2-api.angular-buch.com';
 
-  books: Book[];
+  constructor(private http: HttpClient) {}
 
-  getAll() {
-    return this.books;
+  getAll(): Observable<Book[]> {
+    return this.http.get<BookRaw[]>(`${this.api}/books`).pipe(
+      retry(3),
+      map(rawBooks => rawBooks.map(rawBook => BookFactory.fromObject(rawBook))),
+      catchError(this.errorHandler)
+    );
   }
 
-  getSingle(isbn) {
-    return this.books
-      .find(book => book.isbn === isbn);
+  getSingle(isbn: string): Observable<Book> {
+    return this.http.get<BookRaw>(`${this.api}/book/${isbn}`).pipe(
+      retry(3),
+      map(rawBook => BookFactory.fromObject(rawBook)),
+      catchError(this.errorHandler)
+    );
+  }
+
+  private errorHandler(error: Error | any): Observable<any> {
+    return throwError(error);
   }
 }
